c24b2b28f8e703e3dbf361fcf7a36cecab2d00e4
hamzahashmi3 <technocannons3@gmail.com>
2021-03-26 18:29:26 +0500

    index.js signup/api build

--- Server.js
+++ Server.js
@@ -4,11 +4,12 @@ const path = require('path');
 const app = express();
 
 app.use(compression());
-app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// app.get('*', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 const PORT = process.env.PORT || 3000;
 
